88e390162764319bfa49ac4772b7ac18299b01cc
Paulo Castellano <paulofcastellano@gmail.com>
2020-09-20 20:16:07 -0300

    first commit

---
+++ src/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button class="button" type="button">My Button</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.button {
+  border-radius: 4px;
+  background: #000;
+  color: #fff;
+  padding: 6px 12px;
+}
+</style>
